abcf7f6229d3c7596e448759a7ef03f95b122a1f
Minakshi24902 <mehaktathgur@gmail.com>
2025-05-02 16:49:53 +0530

    update byminak

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,9 @@ import InformationTech from './pages/InformationTech'
 import Agro from './pages/Agro';
 import Sectors from './pages/Sectors';
 import Aboutus from './pages/Aboutus';
-<<<<<<< HEAD
-import Telecom from './pages/Telecom';
-import Tourism from './pages/Tourism'
-=======
 import Education from './pages/Education';
 import Petrol from './pages/Petrol';
 import Health from './pages/Health';
->>>>>>> 36bb1eec88aeb443048fb7f3553fa9ee20c19d00
 
 const App = () => {
   return (
@@ -26,16 +21,6 @@ const App = () => {
     <Navbar/>
     <Router>
     <Routes>
-<<<<<<< HEAD
-      <Route path="/" element={<Home />}           />
-      <Route path="/about" element={<About />}     />                             
-      <Route path="/sectors" element={<Sectors />} />                  
-      <Route path="/Aboutus" element={<Aboutus />} /> 
-      <Route path="/PowerPlant" element={<PowerPlant />}    />  
-      <Route path= "/Telecom" element={<Telecom />}   />   
-      <Route path="/InformationTech" element={<InformationTech />} />  
-      <Route path="/Tourism" element={<Tourism />}  />   
-=======
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       
@@ -45,7 +30,6 @@ const App = () => {
       <Route path="/Education" element={<Education />} />
       <Route path="/Health" element={<Health />} />
       <Route path="/Petrol" element={<Petrol />} />
->>>>>>> 36bb1eec88aeb443048fb7f3553fa9ee20c19d00
 
     </Routes> 
   </Router>
